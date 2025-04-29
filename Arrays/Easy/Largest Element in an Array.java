b44bbfe4d219ea0a5885c78e683b21503317433f
Prachi Arora <123627759+ARORA-PRACHI@users.noreply.github.com>
2025-04-29 14:44:36 +0530

    Array Q1

---
+++ Arrays/Easy/Largest Element in an Array.java
@@ -0,0 +1,13 @@
+// https://www.geeksforgeeks.org/problems/largest-element-in-array4009/1
+class Solution {
+    public static int largest(int[] arr) {
+        int n=arr.length;
+        int max=arr[0];
+        for(int i=0;i<n;i++){
+            if(arr[i]>max){
+                max=arr[i];
+            }
+        }
+        return max;
+    }
+}
