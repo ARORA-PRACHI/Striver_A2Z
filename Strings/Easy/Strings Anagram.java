02118b0c842a242ce0f024d23004145fe0f9d731
Prachi Arora <123627759+ARORA-PRACHI@users.noreply.github.com>
2025-05-13 22:52:53 +0530

    Q 29

---
+++ Strings/Easy/Strings Anagram.java
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/valid-anagram/description/
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()) return false;
+        int [] freq=new int[26];
+        for(char c:s.toCharArray()){
+            freq[c-'a']++;
+        }
+        for(char c:t.toCharArray()){
+            freq[c-'a']--;
+        }
+        for(int i:freq){
+            if(i!=0) return false;
+        }
+        return true;
+    }
+}
