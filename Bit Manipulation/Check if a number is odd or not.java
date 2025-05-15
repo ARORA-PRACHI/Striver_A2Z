dabb4fe98b5310bfa0f0811496880104ba0c97e7
Prachi Arora <123627759+ARORA-PRACHI@users.noreply.github.com>
2025-05-16 02:54:41 +0530

    Q 33

---
+++ Bit Manipulation/Check if a number is odd or not.java
@@ -0,0 +1,7 @@
+// https://www.geeksforgeeks.org/problems/odd-or-even3618/1
+class Solution {
+    static boolean isEven(int n) {
+        if((n & 1)==0) return true;
+        return false;
+    }
+}
