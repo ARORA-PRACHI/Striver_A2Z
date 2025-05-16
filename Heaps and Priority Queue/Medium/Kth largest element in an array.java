ba66a96e168e6302315648a56022a6c7690d2d2f
Prachi Arora <123627759+ARORA-PRACHI@users.noreply.github.com>
2025-05-17 02:29:05 +0530

    Q 38

---
+++ Heaps and Priority Queue/Medium/Kth largest element in an array.java
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/kth-largest-element-in-an-array/description/
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer>pq=new PriorityQueue<>();
+        for(int num:nums){
+            pq.add(num);
+            if(pq.size()>k){
+                pq.poll();
+            }
+        }
+        return pq.peek();
+    }
+}
