09ec7fee24adc2dbde5f573050bb5aafc2d916a9
Prachi Arora <123627759+ARORA-PRACHI@users.noreply.github.com>
2025-05-17 02:37:25 +0530

    Q 39

---
+++ Heaps and Priority Queue/Medium/Kth smallest element in an array.java
@@ -0,0 +1,65 @@
+// https://www.geeksforgeeks.org/problems/kth-smallest-element5635/1
+class Solution {
+    public static int kthSmallest(int[] arr, int k) {
+         PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+
+        for (int num : arr) {
+            maxHeap.add(num);
+            if (maxHeap.size() > k) {
+                maxHeap.poll(); 
+            }
+        }
+        return maxHeap.peek();
+    }
+}
+
+⏱ Time: O(n log k)
+📦 Space: O(k)
+
+or
+  Quick Sort Approach 
+⏱ Time: O(n) average, O(n^2) worst-case
+📦 Space: O(1)
+
+  
+  import java.util.Random;
+
+class Solution {
+    public int kthSmallest(int[] nums, int k) {
+        return quickSelect(nums, 0, nums.length - 1, k - 1);
+    }
+
+    private int quickSelect(int[] nums, int left, int right, int k) {
+        if (left == right) return nums[left];
+
+        int pivotIndex = partition(nums, left, right);
+
+        if (k == pivotIndex) {
+            return nums[k];
+        } else if (k < pivotIndex) {
+            return quickSelect(nums, left, pivotIndex - 1, k);
+        } else {
+            return quickSelect(nums, pivotIndex + 1, right, k);
+        }
+    }
+
+    private int partition(int[] nums, int left, int right) {
+        int pivot = nums[right];
+        int i = left;
+
+        for (int j = left; j < right; j++) {
+            if (nums[j] <= pivot) {
+                swap(nums, i, j);
+                i++;
+            }
+        }
+        swap(nums, i, right);
+        return i;
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+}
