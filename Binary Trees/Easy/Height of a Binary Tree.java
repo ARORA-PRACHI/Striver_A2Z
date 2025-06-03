36d7c2096b87aafde7efc7473c49acedf94f9d41
Prachi Arora <123627759+ARORA-PRACHI@users.noreply.github.com>
2025-06-03 18:49:35 +0530

    Q 46

---
+++ Binary Trees/Easy/Height of a Binary Tree.java
@@ -0,0 +1,10 @@
+https://leetcode.com/problems/maximum-depth-of-binary-tree/description/
+
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if(root==null) return 0;
+        int left=maxDepth(root.left);
+        int right=maxDepth(root.right);
+        return Math.max(left,right)+1;
+    }
+}
